195cf09b3a72588f143f3f055cc08e8e07fd32d8
muddassirkilledar <mkmuddassir22@gmail.com>
2024-12-16 19:25:42 +0530

    content-design page done

--- components/ContentDesignWithImage/index.js
+++ components/ContentDesignWithImage/index.js
@@ -4,10 +4,10 @@ import React from "react";
 
 const ContentDesignWithImage = ({ children, image, isLeft }) => {
     return <div className={`flex w-100 ${isLeft ? "flex-row-reverse" : ""}`}>
-        <div className="bg-[#F2F2F2] px-3 py-5 w-[55%]">
+        <div className="bg-[#F2F2F2] px-3 py-5 w-[45%]">
             {children}
         </div>
-        <div className="w-[45%]"><Image src={image} width={700} height={610} alt="cdimg" /></div>
+        <div className="w-[55%]"><Image src={image} width={700} height={610} alt="cdimg" /></div>
     </div>
 }
 

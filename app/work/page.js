dcec2d20363a25dc959f0684d2e793fe32cb874e
udbhavpushkar <udbhavpushkar@gmail.com>
2024-12-25 13:47:26 +0530

    work page changes

--- app/work/page.js
+++ app/work/page.js
@@ -66,20 +66,20 @@ export default function Work() {
                         content. I often communicate my thoughts through a blend of text and visuals, ensuring clarity and
                         simplicity. Through this approach, I ensure that every piece of content is both impactful and accessible.
                     </div>
-                    <div className="mt-3">
+                    <div className="my-6">
                         <div className="text-3xl font-semibold mb-3">Visual Communication</div>
                         <ImageGallery list={CONTENT_STRATEGIST_1} />
                     </div>
-                    <div className="mt-3 grid grid-cols-3 gap-3">
+                    <div className="my-3 grid grid-cols-3 gap-3">
                         <SquareCarousel images={CONTENT_STRATEGIST_3} />
                         <SquareCarousel images={CONTENT_STRATEGIST_4} />
                         <SquareCarousel images={CONTENT_STRATEGIST_3} />
                     </div>
-                    <div className="mt-3">
-                        <div className="text-3xl font-semibold mb-6">Storytelling (Script Writing | Conceptualisation | Strategy)</div>
+                    <div className="my-3">
+                        <div className="text-3xl font-semibold my-6">Storytelling (Script Writing | Conceptualisation | Strategy)</div>
                         <ImageGallery list={CONTENT_STRATEGIST_2} />
                     </div>
-                    <div className="mt-3">
+                    <div className="my-3">
                         <div className="text-3xl font-semibold my-6">Linkedin Article & blog</div>
                         <div className="grid grid-cols-3 gap-8">
                             {CONTENT_STRATEGIST_5.map((item, index) => (
@@ -93,6 +93,34 @@ export default function Work() {
                                 />
                             ))}
 
+                        </div>
+                    </div>
+                    <div className="my-3">
+                        <div className="text-3xl font-semibold my-6">Branding & Marketing</div>
+                        <Image src="/images/solos.png" width={1325} height={766} alt="solos" />
+                        <div className="text-3xl font-semibold my-6">Brand Name, Tag-line & Branding for an Ice Cream Brand</div>
+                    </div>
+                </div>
+            </div>
+            <div className="w-full bg-[#ECEDFF]">
+                <div className="max-w-[1325px] mx-auto font-crimson text-[#545794] text-center py-3 font-semibold">
+                    Concept | Strategy| Story Boarding | Visualisation | Branding| Content Writing | Content Aesthetics
+                </div>
+            </div>
+            <div className="bg-white w-full">
+                <div className="max-w-[1325px] mx-auto font-crimson py-14">
+                    <div className="text-4xl font-semibold">Work as a Content Creator</div>
+                    <div className="text-2xl my-5">
+                        As a content creator, I’ve had the privilege of collaborating with major brands such as TRESemmé, Dove,
+                        Lakmé, Nivea, and 30+ other brands for Instagram collaboration, UGC content projects, and sponsored ads
+                        content. These collaborations provided me valuable insights into diverse content strategies and brand goals
+                        of famous brands, enhancing my understanding of how different brands approach content creation and engagement.
+                    </div>
+                    <div className="my-6">
+                        <div className="text-3xl font-semibold mb-3">Meta X Lakmé Event</div>
+                        <div className="text-2xl my-5">
+                            Invited to the Meta X Lakmé event as a content creator. Networked and collaborated with various creators,
+                            sharing insights and building connections.
                         </div>
                     </div>
                 </div>
